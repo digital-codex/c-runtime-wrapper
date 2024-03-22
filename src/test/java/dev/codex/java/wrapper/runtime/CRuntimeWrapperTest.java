145c3bbcb16af63e49b206f5279b7fded936c6ec
treyvon <Eugenewhitaker2022@gmail.com>
2024-03-21 22:03:14 -0400

    test(runtime): refactor prints into assertions in test

--- src/test/java/dev/codex/java/wrapper/runtime/CRuntimeWrapperTest.java
+++ src/test/java/dev/codex/java/wrapper/runtime/CRuntimeWrapperTest.java
@@ -1,27 +1,30 @@
 package dev.codex.java.wrapper.runtime;
 
+import dev.codex.java.wrapper.type.Error;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CRuntimeWrapperTest {
+    public static final int MAGIC_NUMBER = 0x7f454c46;
+    public static final byte CLASS = 0x02;
 
     @Test
-    void test_parse_sh_executable_then_print_magic_and_class() {
+    void test_parse_sh_executable_then_assert_magic_and_class() {
         try (FileStream fp = CRuntimeWrapper.fopen("/bin/sh", AccessMode.READ)) {
             byte[] buffer = new byte[4];
 
             long ret = CRuntimeWrapper.fread(buffer, buffer.length, 1, fp);
             assertEquals(1, ret, "fread() failed: " + ret);
 
-            System.out.printf("ELF magic %#04x%02x%02x%02x%n", buffer[0], buffer[1], buffer[2], buffer[3]);
+            assertEquals(CRuntimeWrapperTest.MAGIC_NUMBER, buffer[3] | (buffer[2] << 8) | (buffer[1] << 16) | (buffer[0] << 24));
 
             ret = CRuntimeWrapper.fread(buffer, 1, 1, fp);
             assertEquals(1, ret, "fread() failed: " + ret);
 
-            System.out.printf("Class: %#04x\n", buffer[0]);
+            assertEquals(CRuntimeWrapperTest.CLASS, buffer[0]);
         } catch (Error error) {
-            fail();
+            fail(error.getMessage());
         }
     }
 }
